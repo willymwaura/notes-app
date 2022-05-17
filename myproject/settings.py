777ec032b325769ff19649179f0ffac3d7f51239
willymwaura <wilsonmwaura697@gmail.com>
2022-05-18 05:01:42 +0900

    second commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -77,15 +77,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'my_project',
-        'USER': 'root',
-        'PASSWORD':'password',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
